2c622c512175c2dab87759e4dc730de4c7e652e4
Carles Mateu <carlesm@carlesm.com>
2023-02-20 10:49:11 +0000

    Downloading web

--- web1.py
+++ web1.py
@@ -1,12 +1,26 @@
 
+import urllib3
 
 class Client(object):
 
     def __init__(self) -> None:
-        pass
+        self.url = "https://www.99-bottles-of-beer.net/language-python-808.html"
+    
+    def get_web(self):
+        # connect url
+        httppool = urllib3.PoolManager()
+        # get html
+        resultat = httppool.request("GET", self.url)
+        # retorn html
+        return resultat.data.decode('utf-8')
+        
 
     def get_data(self):
-        pass
+        # descarregar-se web
+        dades = self.get_web()
+        # llegir html
+        # retornar dades
+        return dades
 
 if __name__=="__main__":
     client = Client()
