d2926f94f66ddac0ff15f0ffce5c259cbb130148
Carles Mateu <carlesm@carlesm.com>
2023-02-20 11:36:37 +0000

    Done

--- api1.py
+++ api1.py
@@ -1,11 +1,30 @@
 #  http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=10
 
+import urllib.request
+import xmltodict
+import pprint
 
 class Client(object):
 
     def __init__(self) -> None:
         self.url = "http://export.arxiv.org/api/query?start=0&max_results=10&search_query=all:"
 
+    def get_api(self, query):
+        url_file = urllib.request.urlopen(self.url+query)
+        xmldata = url_file.read().decode('utf-8')
+        return xmldata
+
+    def parse_xml(self, xmldata):
+        dades = xmltodict.parse(xmldata)
+        return dades
+
+    def select_data(self, data):
+        dades = []
+        for e in data['feed']['entry']:
+            title = e['title']
+            dades.append(title)
+        return dades
+        # dades = [e['title'] for e in data['feed']['entry']]
 
     def get_data(self, query):
         # descarregar-se dades
@@ -19,4 +38,4 @@ class Client(object):
 if __name__=="__main__":
     client = Client()
     dades = client.get_data("lstm")
-    print(dades)
+    pprint.pprint(dades)
