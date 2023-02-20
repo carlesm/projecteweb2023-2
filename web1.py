b7525c3b4fc340d9519365f8a87ebc16ac29f8a6
Carles Mateu <carlesm@carlesm.com>
2023-02-20 10:57:53 +0000

    Parse BS4

--- web1.py
+++ web1.py
@@ -13,12 +13,23 @@ class Client(object):
         resultat = httppool.request("GET", self.url)
         # retorn html
         return resultat.data.decode('utf-8')
-        
+
+    def parse_html(self, html):
+        # convertir bs4
+        soup = bs4.BeautifulSoup(html, features="html.parser")
+        # buscar bs4 -> codi
+        element_div = soup.find_all('div', attrs={"id":"main"})[0]
+        codi = element_div.find_all('pre')[0]
+        # retornar codi
+        return codi.text
+                
+
 
     def get_data(self):
         # descarregar-se web
         dades = self.get_web()
         # llegir html
+        dades = self.parse_html(dades)
         # retornar dades
         return dades
 
